82e11eb30b99197ebbdd1e1ad28c85085a382415
UKSGNATHAN <53213900+UKSGNATHAN@users.noreply.github.com>
2020-07-01 17:48:56 +0530

    Create tomcatdeploy.groovy

---
+++ vars/tomcatdeploy.groovy
@@ -0,0 +1,19 @@
+def call(config){
+  def credId = config['credId']
+  def ip = config['ip']
+  def userName = config['userName']
+  def tomcatHome = config['tomcatHome']
+  
+  sshagent([credId]) {
+                    // Stop the tomcat //
+                    sh "ssh -o StrictHostKeyChecking=no ${userName}@${ip} ${tomcatHome}/bin/shutdown.sh"
+                    // Delete old warfile //
+                    sh "ssh ${username}@${ip} rm -rf ${tomcatHome}/webapps/${warName}*"
+                    // Copy latest war file to tomcat-dev server //
+                    sh "scp target/springvmvc.war ${userName}@${ip}:${tomcatHome}/webapps/"
+                    // start the server //
+                    sh "ssh ${username}@${ip} ${tomcatHome}/bin/startup.sh"
+   }
+ }
+              
+                 
